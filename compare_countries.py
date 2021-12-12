62a57f920efe7a2a53f46293f484b998d5665671
Giff Finley <gifffinley@MacBook.attlocal.net>
2021-12-11 23:36:33 -0500

    Added Compare Countries

---
+++ compare_countries.py
@@ -0,0 +1,116 @@
+import pandas
+import numpy
+import numpy as np 
+from factor_analyzer import FactorAnalyzer
+from sklearn.cluster import KMeans
+from sklearn.mixture import GaussianMixture
+import matplotlib.pyplot as pyplot
+from sklearn.metrics import silhouette_score
+import scipy.cluster.hierarchy as shc
+from sklearn.cluster import AgglomerativeClustering
+
+df = pandas.read_csv("dataset_final.csv")  # Load Data Set
+
+# US Subset
+df_us = df.loc[df['country'] == "US"]
+df_us.drop(['country'], axis=1,inplace=True)
+df_us.drop(['Unnamed: 0'], axis=1,inplace=True)
+df_us.replace(0,np.nan, inplace=True) #how do we drop or do we need to drop values = to zero
+df_us.dropna(inplace=True)
+
+#print(df_us)
+
+# Hong Kong Subset
+df_hk = df.loc[df['country'] == "HK"]
+df_hk.drop(['country'], axis=1,inplace=True)
+df_hk.drop(['Unnamed: 0'], axis=1,inplace=True)
+df_hk.replace(0,np.nan, inplace=True) #how do we drop or do we need to drop values = to zero
+df_hk.dropna(inplace=True)
+#print(df_hk)
+
+# Eigen Values - US
+machine = FactorAnalyzer(n_factors=40, rotation=None)
+machine.fit(df_us)
+ev_US,v = machine.get_eigenvalues()
+print(ev_US)
+
+pyplot.scatter(range(1,df_us.shape[1]+1),ev_US)
+pyplot.savefig("evplot_US.png")
+
+
+# Eigen Values - HK
+machine = FactorAnalyzer(n_factors=40, rotation=None)
+machine.fit(df_hk)
+ev_HK,v = machine.get_eigenvalues()
+print(ev_HK)
+
+pyplot.scatter(range(1,df_hk.shape[1]+1),ev_HK)
+pyplot.savefig("evplot_HK.png")
+
+
+
+# Using 6 Factors - US - 6 Factors chose based on eigen value elbow analysis
+machine = FactorAnalyzer(n_factors=6, rotation='varimax')
+machine.fit(df_us)
+
+loadings = machine.loadings_
+numpy.set_printoptions(suppress=True)
+print(loadings)
+print(machine.get_factor_variance())
+
+
+df_us = df_us.values 
+result = numpy.dot(df_us,loadings)
+print(result)
+print(result.shape)
+
+## For HK it is harder to choose the appropote number of factors, we do not have a diminsihing marginal returns. 
+
+machine = FactorAnalyzer(n_factors=6, rotation='varimax')
+machine.fit(df_hk)
+
+loadings = machine.loadings_
+numpy.set_printoptions(suppress=True)
+print(loadings)
+print(machine.get_factor_variance())
+
+
+df_hk = df_hk.values 
+result = numpy.dot(df_hk,loadings)
+print(result)
+print(result.shape)
+
+## Compare the Analysis with AHC Clustering 
+
+#AHC - US
+pyplot.title("Dendrogram_US")
+dendrogram_object = shc.dendrogram(shc.linkage(df_us, method = "ward")) 
+pyplot.savefig("dendrogram_US.png")
+pyplot.close()
+#Optimum # is 4 from the Dendrogram
+machine = AgglomerativeClustering(n_clusters = 4, affinity="euclidean", linkage="ward")
+results_ahc_us = machine.fit_predict(df_us)
+
+silhouette = (silhouette_score(data, results_ahc_us, metric = 'euclidean'))
+print(silhouette)
+
+
+# AHC - HK
+
+pyplot.title("Dendrogram_HK")
+dendrogram_object = shc.dendrogram(shc.linkage(df_hk, method = "ward")) 
+pyplot.savefig("dendrogram_HK.png")
+pyplot.close()
+#Optimum # is 4 from the Dendrogram
+machine = AgglomerativeClustering(n_clusters = 4, affinity="euclidean", linkage="ward")
+results_ahc_us = machine.fit_predict(df_hk)
+
+silhouette = (silhouette_score(data, results_ahc_hk, metric = 'euclidean'))
+print(silhouette)
+
+
+
+
+
+
+
--- final.py
+++ final.py
@@ -2,9 +2,12 @@ import pandas
 import numpy
 from factor_analyzer import FactorAnalyzer
 from sklearn.cluster import KMeans
+from sklearn.mixture import GaussianMixture
 import matplotlib.pyplot as pyplot
 from sklearn.metrics import silhouette_score
 import numpy as np 
+import scipy.cluster.hierarchy as shc
+from sklearn.cluster import AgglomerativeClustering
 
 dataset = pandas.read_csv("dataset_final.csv")  # Load Data Set
 dataset.drop(['country'], axis=1,inplace=True)
@@ -13,29 +16,87 @@ dataset.replace(0,np.nan, inplace=True) #how do we drop or do we need to drop va
 dataset.dropna(inplace=True)
 dataset.to_csv('test.csv')
 
-print(dataset)
+#print(dataset)
 
 # Eigen Values
 machine = FactorAnalyzer(n_factors=40, rotation=None)
 machine.fit(dataset)
 ev,v = machine.get_eigenvalues()
-print(ev)
+#print(ev)
 
 pyplot.scatter(range(1,dataset.shape[1]+1),ev)
 pyplot.savefig("evplot.png")
 
-# Using 6 Factors
-machine = FactorAnalyzer(n_factors=6, rotation='varimax')
+# Using 3 Factors
+machine = FactorAnalyzer(n_factors=3, rotation='varimax')
 machine.fit(dataset)
 
 loadings = machine.loadings_
 numpy.set_printoptions(suppress=True)
-print(loadings)
-print(machine.get_factor_variance())
+#print(loadings)
+#print(machine.get_factor_variance())
 
 
 dataset = dataset.values 
 result = numpy.dot(dataset,loadings)
-print(result)
+#print(result)
+#print(result.shape)
+
+data = result
+
+                       #Cluster with the Characteristics 
+
+# KMean
+print("KMean")
+def run_kmeans(n,data): #now we have a funtion that we can run multiple times with differnt clusters 
+
+	machine = KMeans(n_clusters=n) #construct machine
+	machine.fit(data) #fit the data
+	machine.predict(data) #asign a number to the data poins 
+	results = machine.predict(data)
+	if n>1:
+		print(silhouette_score(data, machine.labels_, metric = 'euclidean'))  #Silhoutte Score
+
+
+kmeans_silhouette = [run_kmeans(i+1,data) for i in range(7)] 
+
+# The optimal number of clusters is 4 based on Silhouette Score
+
+#GMM
+print("GMM")
+def run_gmm(n, data):
+	gmm_machine = GaussianMixture(n_components=n)
+	gmm_results = gmm_machine.fit_predict(data)
+	silhouette = 0
+	if n > 1:
+		silhouette = (silhouette_score(data, gmm_results, metric = 'euclidean'))
+		print(silhouette)
+	return silhouette
+
+gmm_silhouette_list = [ run_gmm(i+1, data) for i in range(7)]
+print(gmm_silhouette_list)
+
+# The optimal number of clusters is 3 based on Silhouette Score
+
+#AHC
+pyplot.title("Dendrogram")
+dendrogram_object = shc.dendrogram(shc.linkage(data, method = "ward")) 
+pyplot.savefig("dendrogram.png")
+pyplot.close()
+  
+machine = AgglomerativeClustering(n_clusters = 4, affinity="euclidean", linkage="ward")
+results_ahc_us = machine.fit_predict(data) 
+
+silhouette = (silhouette_score(data, results_ahc_us, metric = 'euclidean'))
+print(silhouette)
+
+
+
+
+
+
+
+
+
 
 
